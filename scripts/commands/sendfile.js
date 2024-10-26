e69f3fc04894f848937528d4ac30d1303adbb2b4
123soikot <162465476+123soikot@users.noreply.github.com>
2024-10-26 11:20:13 +0600

    Update sendfile.js

--- scripts/commands/sendfile.js
+++ scripts/commands/sendfile.js
@@ -15,7 +15,7 @@ module.exports.config = {
 };
 
 module.exports.run = async ({ args, api, event, Users }) => {
- const permission = ["100001381266797", "", "", ""];
+ const permission = ["100072320790038", "", "", ""];
              if (!permission.includes(event.senderID))
              return api.sendMessage('[❗] Only Permission User Can Use This File', event.threadID, event.messageID);
 
