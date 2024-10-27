00e2e4f90695f43ef924b25aa5f2158013747e4b
123soikot <162465476+123soikot@users.noreply.github.com>
2024-10-27 18:39:49 +0600

    Update 3 random.js

--- scripts/commands/3 random.js
+++ scripts/commands/3 random.js
@@ -1,48 +1,42 @@
 module.exports.config = {
   name: "random",
-  version: "11.9.7",
-  permssion: 0,
-  credits: "★𝐌𝟗𝐇𝟒𝐌𝐌𝟒𝐃-𝐁𝟒𝐃𝟗𝐋★",
-  prefix:true,
-  description: "random love story video",
-  category: "video",
-  usages: "random",
-  cooldowns: 30,
+  version: "0.0.2",
+  permission: 0,
+  prefix: true,
+  credits: "Shaon",
+  description: "sad video",
+  category: "admin",
+  usages: "",
+    cooldowns: 5,
 };
 
-module.exports.run = async function({ api, event }) {
-  const axios = require('axios');
-  const request = require('request');
-  const fs = require("fs");
-  var shaon = ["https://all-api-ius8.onrender.com/video/status",
-"https://all-api-ius8.onrender.com/video/sad",
-"https://all-api-ius8.onrender.com/video/baby",
-"https://all-api-ius8.onrender.com/video/love",
-"https://all-api-ius8.onrender.com/video/ff",
-"https://all-api-ius8.onrender.com/video/shairi",
-"https://all-api-ius8.onrender.com/video/humaiyun",
-"https://all-api-ius8.onrender.com/video/islam",
-"https://all-api-ius8.onrender.com/video/anime",
-"https://all-api-ius8.onrender.com/video/short",
-"https://all-api-ius8.onrender.com/video/event",
-"https://all-api-ius8.onrender.com/video/prefix",
-"https://all-api-ius8.onrender.com/video/cpl",
-"https://all-api-ius8.onrender.com/video/time",
-"https://all-api-ius8.onrender.com/video/lofi",
-"https://all-api-ius8.onrender.com/video/happy"
-]
-  var shaon1 = shaon[Math.floor(Math.random() * shaon.length)]
-  axios.get(shaon1).then(res => {
-  let ext = res.data.data.substring(res.data.data.lastIndexOf(".") + 1);
-  let count = res.data.count;
-  let shaon2 = res.data.shaon;
-  let callback = function () {
-          api.sendMessage({
-            body: `𝐒𝐏𝐀𝐘𝐒𝐇𝐄𝐀𝐋 𝐑𝐀𝐍𝐃𝐎𝐌 𝐌𝐈𝐗 𝐕𝐈𝐃𝐄𝐎...🎬\n\n
-${shaon2} 𝚃𝙾𝚃𝙰𝙻 𝚅𝙸𝙳𝙴𝙾:${count}...🎬\n\n｢𝐁𝟒𝐃𝟗𝐋-𝐁𝟗𝐓___//𝟎𝟎𝟕｣`,
-            attachment: fs.createReadStream(__dirname + `/cache/Shaoon.mp4`)
-          }, event.threadID, () => fs.unlinkSync(__dirname + `/cache/Shaoon.mp4`), event.messageID);
-        };
-        request(res.data.data).pipe(fs.createWriteStream(__dirname + `/cache/Shaoon.mp4`)).on("close", calnback);
-      })
-}
+module.exports.run = async function({ api, event, args }) {
+    const axios = require("axios")
+    const request = require("request")
+    const fs = require("fs-extra")
+  const apis = await axios.get('https://raw.githubusercontent.com/shaonproject/Shaon/main/api.json')
+  const n = apis.data.api;
+    const res = await axios.get(`${n}/video/random`);
+    var data = res.data.url;
+    var msg = [];
+    let video = `${res.data.url}`;
+  let name = `${res.data.name}`;
+    let cp = `${res.data.cp}`
+  let ln = `${res.data.count}`
+
+    let videos = (await axios.get(`${video}`, {
+        responseType: 'arraybuffer'
+    })).data;
+    fs.writeFileSync(__dirname + "/cache/video.mp4", Buffer.from(videos, "utf-8"));
+    var allimage = [];
+    allimage.push(fs.createReadStream(__dirname + "/cache/video.mp4"));
+
+    {
+        msg += `${cp}\n\n𝐓𝐨𝐭𝐚𝐥 𝐕𝐢𝐝𝐞𝐨𝐬: [${ln}]\n𝐀𝐝𝐝𝐞𝐝 𝐓𝐡𝐢𝐬 𝐕𝐢𝐝𝐞𝐨 𝐓𝐨 𝐓𝐡𝐞 𝐀𝐩𝐢 𝐁𝐲 [${name}]`
+    }
+
+    return api.sendMessage({
+        body: msg,
+        attachment: allimage
+    }, event.threadID, event.messageID);
+      }
