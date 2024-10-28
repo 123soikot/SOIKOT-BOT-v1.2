1c204ad4b09680765640d513e9fc8536d1b7bb92
123soikot <162465476+123soikot@users.noreply.github.com>
2024-10-28 17:32:21 +0600

    Create gail.js

---
+++ scripts/commands/gail.js
@@ -0,0 +1,24 @@
+const fs = require("fs");
+module.exports.config = {
+	name: "gali",
+    version: "1.0.1",
+	hasPermssion: 0,
+	credits: "SoiKOt", 
+	description: "no prefix",
+	commandCategory: "no prefix",
+	usages: "abal",
+    cooldowns: 5, 
+};
+
+module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
+	var { threadID, messageID } = event;
+	if (event.body.indexOf("fuck")==0 || event.body.indexOf("mc")==0 || event.body.indexOf("chod")==0 || event.body.indexOf("bal")==0 || event.body.indexOf("bc")==0 || event.body.indexOf("maa ki chut")==0 || event.body.indexOf("xod")==0 || event.body.indexOf("behen chod")==0 || event.body.indexOf("sudi")==0 || event.body.indexOf("madarchod")==0 || event.body.indexOf("chudi")==0 || event.body.indexOf("gala gali")==0) {
+		var msg = {
+				body: "(  আমার Boss EX卝 বয়ফ্রেন্ডヅ কে ডেকে দিবো গালি তোমার পু****কি ভিতর ডুকে দিবে...📌)",
+			}
+			api.sendMessage(msg, threadID, messageID);
+		}
+	}
+	module.exports.run = function({ api, event, client, __GLOBAL }) {
+
+  }
